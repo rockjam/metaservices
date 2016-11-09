38364e63fc41faf527ba361747b6a8650f48913b
rockjam <rockjam@actor.im>
2016-11-09 11:46:55 +0300

    chore: upgrade sbt-fresh

--- project/Build.scala
+++ project/Build.scala
@@ -21,30 +21,30 @@ object Build extends AutoPlugin {
   override def projectSettings =
     reformatOnCompileSettings ++
     Vector(
-           // Core settings
-           organization := "com.github.rockjam", 
-           licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
-           mappings.in(Compile, packageBin) += baseDirectory.in(ThisBuild).value / "LICENSE" -> "LICENSE",
-           scalaVersion := Version.Scala,
-           crossScalaVersions := Vector(scalaVersion.value),
-           scalacOptions ++= Vector(
-             "-unchecked",
-             "-deprecation",
-             "-language:_",
-             "-target:jvm-1.8",
-             "-encoding", "UTF-8"
-           ),
-           unmanagedSourceDirectories.in(Compile) := Vector(scalaSource.in(Compile).value),
-           unmanagedSourceDirectories.in(Test) := Vector(scalaSource.in(Test).value),
+      // Core settings
+      organization := "com.github.rockjam",
+      licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
+      mappings.in(Compile, packageBin) += baseDirectory.in(ThisBuild).value / "LICENSE" -> "LICENSE",
+      scalaVersion := Version.Scala,
+      crossScalaVersions := Vector(scalaVersion.value),
+      scalacOptions ++= Vector(
+        "-unchecked",
+        "-deprecation",
+        "-language:_",
+        "-target:jvm-1.8",
+        "-encoding", "UTF-8"
+      ),
+      unmanagedSourceDirectories.in(Compile) := Vector(scalaSource.in(Compile).value),
+      unmanagedSourceDirectories.in(Test) := Vector(scalaSource.in(Test).value),
 
-           // scalafmt settings
-           formatSbtFiles := false,
-           scalafmtConfig := Some(baseDirectory.in(ThisBuild).value / ".scalafmt"),
+      // scalafmt settings
+      formatSbtFiles := false,
+      scalafmtConfig := Some(baseDirectory.in(ThisBuild).value / ".scalafmt.conf"),
 
-           // Git settings
-           git.useGitDescribe := true,
+      // Git settings
+      git.useGitDescribe := true,
 
-           // Header settings
-           headers := Map("scala" -> Apache2_0("2016", "Nikolay Tatarinov"))
+      // Header settings
+      headers := Map("scala" -> Apache2_0("2016", "Nikolay Tatarinov"))
     )
 }
